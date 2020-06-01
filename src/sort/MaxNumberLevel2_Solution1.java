252c63961cb2a05ac92e22b44bca3b334adeebf2
ingbeen <ingbeen@naver.com>
2020-06-01 09:05:10 +0900

    CompleteSearch

--- src/sort/MaxNumberLevel2_Solution1.java
+++ src/sort/MaxNumberLevel2_Solution1.java
@@ -10,17 +10,20 @@ int[] numbers = {3, 30, 34, 5, 9};
 
 public class MaxNumberLevel2_Solution1 {
 	public static String solution(int[] numbers) {
-		String answer = ""; // 리턴값 초기화
-		String[] str_numbers = new String[numbers.length]; // 파라미터 numbers 배열의 길이와 같은 string 배열을 생성
+		String answer = "";
+		
+		// 파라미터 numbers 배열의 길이와 같은 string 배열을 생성
+		String[] str_numbers = new String[numbers.length];
 
 		// int 배열인 numbers를 string배열인 str_numbers로 변환
         for (int i = 0 ; i < numbers.length ; i++) {
             str_numbers[i] = String.valueOf(numbers[i]);
         }
 
-     // str_numbers를 Compare()에 정의된 방식으로 정렬
+        // str_numbers를 Compare()에 정의된 방식으로 정렬
         Arrays.sort(str_numbers, new Compare());
-     // 현재 str_numbers는 올림차순으로 정렬되어 있음
+        
+        // 현재 str_numbers는 올림차순으로 정렬되어 있음
         // 따라서 가장 마지막 인덱스의 값이 max값이고, 이것이 반환값의 가장 앞에 올 숫자임
         // 반환값의 맨 앞 숫자가 0이면 answer=0 
         // 반환값의 맨 앞 숫자가 0이 아니면 내림차순으로(가장 큰 수부터) answer에 담음
